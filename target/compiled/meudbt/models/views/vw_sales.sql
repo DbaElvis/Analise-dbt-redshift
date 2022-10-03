aead59095ee3cd8101fbb92f64c86ad8a4aaf56c
DbaElvis <dbaelvis@gmail.com>
2022-10-02 22:01:04 -0300

    Views e tabelas

---
+++ target/compiled/meudbt/models/views/vw_sales.sql
@@ -0,0 +1,22 @@
+with source_sales as (
+    select *
+    from sales
+),
+
+renamed as (
+    select 
+    SALESID AS ID_VENDA,
+    LISTID AS ID_LISTA,
+    SELLERID AS ID_VENDEDOR,
+    BUYERID AS ID_COMPRADOR,
+    EVENTID AS ID_EVENTO,
+    DATEID AS ID_DATE,
+    QTYSOLD AS QUAQNTIDADE_VENDIDA,
+    PRICEPAID AS VALOR_PAGO,
+    COMMISSION AS COMISSAO,
+    SALETIME AS DATA_VENDA
+    from source_sales
+
+)
+
+select * from renamed
